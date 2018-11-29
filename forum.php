806762ab1c880d5a432e6a076261f731f6a0dc05
TrevorSheridan1 <45073443+TrevorSheridan1@users.noreply.github.com>
2018-11-29 12:34:47 -0500

    Update forum.php

--- forum.php
+++ forum.php
@@ -1,10 +1,12 @@
 <!doctype html>
 <?php
-    require "includes/header.php";
+    require "Includes/header.php";
 ?>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+
 	</head>
 	<body>
     	<img src="img/jesuschurch.jpg" class="responsive" alt="Church worship scene" height="400" width="900">
