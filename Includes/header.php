f09d879568b9a8d4d771ccaabdf117664d4c234f
TrevorSheridan1 <45073443+TrevorSheridan1@users.noreply.github.com>
2018-11-29 12:50:43 -0500

    Update header.php

--- Includes/header.php
+++ Includes/header.php
@@ -14,43 +14,45 @@
     </head>
 
     <body>
-        <header>
-            <div class="nav-header-main">
-		<div class="nav_bar">
-			<a class="nav_left" href="./index.php">
-				<img src="img/wave.png" alt="logo goes here">
-			</a>
-					
-               		<div class="nav_right">
-				<ul class="nav_right_list">
-					<li><a href="./index.php">Home</a></li>		
-					<li><a href="./about.php">About</a></li>
-					<li><a href="./events.php">Events</a></li>
-					<li><a href="./contact.php">Contact</a></li>
-				</ul>
-			</div>
-           	</div>
-	</div>
-		<div class="header-login">
-              	<?php 
-                	if(!isset($_SESSION['loggedin'])) {
-				echo '<form action="Includes/login.php" method="post">
-					<input type="text" name="username" placeholder="Username...">
-					<input type="password" name="password" placeholder="Password..."> 
-					<button type="submit" name="login-submit">Login</button>
-				      </form>
+	<div class="navigation-header-container>					     
+		<header>
+		    <div class="nav-header-main">
+			<div class="nav_bar">
+				<a class="nav_left" href="./index.php">
+					<img src="img/wave.png" alt="logo goes here">
+				</a>
 
-                            <form method="post" action="Includes/signup.php">
-                            <button type="submit" name="signup-submit">Sign Up</button></form>';
-                            
-                        } else if ($_SESSION['loggedin'] == true) {
-                            echo '<form action="Includes/logout.php" method="post">
-                            <button type="submit" name="logout-submit">Logout</button>
-                            </form>';                        
-			}
-                    ?>
-                </div>
-        </header>
+				<div class="nav_right">
+					<ul class="nav_right_list">
+						<li><a href="./index.php">Home</a></li>		
+						<li><a href="./about.php">About</a></li>
+						<li><a href="./events.php">Events</a></li>
+						<li><a href="./contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+			<div class="header-login">
+			<?php 
+				if(!isset($_SESSION['loggedin'])) {
+					echo '<form action="Includes/login.php" method="post">
+						<input type="text" name="username" placeholder="Username...">
+						<input type="password" name="password" placeholder="Password..."> 
+						<button type="submit" name="login-submit">Login</button>
+					      </form>
+
+				    <form method="post" action="Includes/signup.php">
+				    <button type="submit" name="signup-submit">Sign Up</button></form>';
+
+				} else if ($_SESSION['loggedin'] == true) {
+				    echo '<form action="Includes/logout.php" method="post">
+				    <button type="submit" name="logout-submit">Logout</button>
+				    </form>';                        
+				}
+			    ?>
+			</div>
+		</header>
+	    </div>
 	</body>
 </html>
          
