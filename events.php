8528a7839f38b64dfa94a8b39150201b72dd3cdb
TrevorSheridan1 <45073443+TrevorSheridan1@users.noreply.github.com>
2018-11-29 16:15:52 -0500

    Update events.php

--- events.php
+++ events.php
@@ -6,9 +6,8 @@
 
 <html>
 	<head>
-		<link rel-"stylesheet" type="text/css" href="css/reset.css">
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
-		<!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+		<!-- <link rel="stylesheet" type="text/css" href="css/style.css">-->
 		<link rel="stylesheet" type="text/css" href="css/events_styles.css">
 	</head>
 	
@@ -16,15 +15,15 @@
 		<div class="events-grid-container">
 			<div class="events-main-content">
 				<div class="events-page-header">
-					<h1 class="events-page-title">Group Up's &amp; Meetings</h1>
+					<h1 class="events-page-title">Meetings &amp; Events</h1>
 				</div>
 
-				<p class="lead">Here is where you can check out our meeting topics with additional information. Make sure to put these important dates in your calendar. As for the meetings, you "Gotta catch 'em' all!", or at least try.</p>
+				<p class="lead">Here is where you can check out our future events and meeting topics with additional information. Make sure to put these important dates in your calendar and be on the look out for anything new. For the events, you "Gotta catch 'em all!", or at least try.</p>
 
 				<div class="grid-item events-articles">
 					<article>
 						<header class="events-headers">
-							<h2 class="events-h2">Class Introduction Presentation (12/6/2018)</h2>
+							<h2 class="events-h2">Class Introduction Presentation (12/06/2018)</h2>
 						</header>
 						<section class="post-content">
 							This week the founding members of our Pokémon club will be presenting their project 4 to their server-side scripting class with professor Fanfarelli. The presentation is one of many being presented in class that day, so come ready to learn. Presentations will begin at 8:00am sharp, so don't be late. 
@@ -39,7 +38,7 @@
 							<h2 class="events-h2">Post Presentation Meetup (12/09/2018)</h2>
 						</header>
 						<section class="post-content">
-							The sunday following the Class Introuction Presentation all members are welcome to group up and join us in a Pokémon hunt around UCF. Members are encouraged to bring thier phones with Pokémon Go lready installed and ready to use, otherwise, you'll have a very difficult time catching the Pokémon.
+							The sunday following the Class Introuction Presentation all members are welcome to group up and join us in a Pokémon hunt around UCF. Members are encouraged to bring thier phones with Pokémon GO lready installed and ready to use, otherwise, you'll have a very difficult time catching the Pokémon.
 						</section>
 						<footer></footer>						
 					</article>
@@ -48,10 +47,10 @@
 				<div class="grid-item events-articles">
 					<article>
 						<header class="events-headers">
-							<h2 class="events-h2">To Be Announced</h2>
+							<h2 class="events-h2">Winter Break (12/07/2018 - 01/07/2019)</h2>
 						</header>
 						<section class="post-content">
-							These words here will be filled in later. Please come back at another time. 
+							Enjoy your holiday break from school. If you are in town, we will be holding one or two events during the winter break, nothing too large since a majority of members will not be around. If you would like to come out to these events and participate please bring and sign the libality waiver with you since we cannot conduct our events on UCF campus and it will be an outdoors event. 
 						</section>
 						<footer></footer>						
 					</article>
@@ -60,10 +59,10 @@
 				<div class="grid-item events-articles">
 					<article>
 						<header class="events-headers">
-							<h2 class="events-h2">To Be Announced</h2>
+							<h2 class="events-h2">"Walking in a Winter Poké-land!" (12/21/2018)</h2>
 						</header>
 						<section class="post-content">
-							These words here will be filled in later. Please come back at another time. 
+							We will be hosting an off-campus event on the 21st of December where we will be walking, and playing Pokémon GO, for charity. The objective of this event is to raise money for the Toys for Tots program by walking around for as long as we can, but as a little twist we will also be playing Pokémon GO simultaneously. Please come out to this event if you can, its for a great cause. Feel free to also bring any physical gifts you can donate to Toys for Tots, as there will be a truck there collecting them. As a bonus, all member of the UCF Pokémon Club who come out and walk a minimum of 3 miles will recieve a free semester of club dues.  
 						</section>
 						<footer></footer>
 					</article>
@@ -72,10 +71,30 @@
 				<div class="grid-item events-articles">				
 					<article>
 						<header class="events-headers">
-							<h2 class="events-h2">To Be Announced</h2>
+							<h2 class="events-h2">Unification for the Spring Semester Revolt [USSR] (01/22/2019)</h2>
 						</header>
 						<section class="post-content">
-							These words here will be filled in later. Please come back at another time. 
+							Two weeks after the sping 2019 semester begins we will be holding a unification rally between our club and the other gaming related clubs on campus. Our objective is to gain awarness for all gaming related incidents that happen on campus as a result of there being a lack of UCF admmistrative regulations in place protecting our rights to game freely. Incidents such as a power surges destroying ones console because of faulty electrical wiring in the old UCF buildings, or even a tree running into a Nintendo Switch player because it's never the other way around, it's always the trees fault. Why would UCF landscaping put a tree right in the middle of a premium gaming location? Neither of these horrfic tragedies should ever happen to a gamer. We will stand against the naysayers who claim that we contribute nothing to the campus as a whole. <span class="span1">We will Unionize, we will mobolize, &amp; we will terrorize</span> all who stand between us and our goal. 
+							<br />
+							Come rally together on the 22nd of February ready to march on Millican Hall, armed with your First Amendment Rights and your cellphones, so that you can record all of it and post it on the internet later. Dont forget to use #gamersToo and #RiseUpGamers when posting to social media. 
+							
+							<br />
+							<br />
+							
+							<span class="span2">This is a no violence &amp; no hostility rally. Those who oppose the peace will be forcibly removed and prosecuted within the legal confines of the law.</span>
+							
+						</section>
+						<footer></footer>						
+					</article>
+				</div>
+				
+				<div class="grid-item events-articles">				
+					<article>
+						<header class="events-headers">
+							<h2 class="events-h2">Valentines Voyage &amp; Beginners Workshop (02/14/2019)</h2>
+						</header>
+						<section class="post-content">
+							For those of you who are new to Pokémon GO or are still stuck at the beginner level, this is a great event for you to attend. We will have challanges and events at multiple skill ranges, including starter and beginner levels, so come on out and win some prizes. Arrive equipped with your phone and Pokémon GO pre-installed for the best experience. For those more advanced, bring us your best Fairy type Pokémon to compete against other Fairy types for the gym all in good spirit for Valentines day. The club member who keeps hold of the gym the longest wins a free copy of Pokémon: Let's Go, Pikachu! or Let's Go, Eevee!, winners choice. 
 						</section>
 						<footer></footer>						
 					</article>
@@ -84,14 +103,9 @@
 			
 			<div class="events-sidebar">
 				<div class="grid-item events-images">
-					<img src="img/gengar.png" alt="gengar">
+					<img src="https://vignette.wikia.nocookie.net/sonicpokemon/images/9/95/Gengar_AG_anime.png/revision/latest?cb=20130620042420img/gengar.png" alt="gengar">
 				</div>
-				
-				<div class="events-sidebar"></div>
-			
-			
-			</div>
-			
+			</div>			
 			<?php
 				require "includes/footer.php";
 			?>
