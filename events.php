a64c6a4397b3581eb6a2aa3cf8f1dad5942ba1d6
TrevorSheridan1 <45073443+TrevorSheridan1@users.noreply.github.com>
2018-11-29 12:18:17 -0500

    Update events.php

--- events.php
+++ events.php
@@ -2,18 +2,99 @@
 <?php
     require "includes/header.php";
 ?>
+
+
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link rel="stylesheet" type="text/css" href="css/events_styles.css">
 	</head>
-	<body>
-    	<img src="img/jesuschurch.jpg" class="responsive" alt="Church worship scene" height="400" width="900">
-
-		<h1>Join Us Sunday's @ 10:00am</h1>
 	
-<?php
-    require "includes/footer.php";
-?>
+	<body>
+		<div class="events-grid-container">
+			<div class="events-main-content">
+				<div class="events-page-header">
+					<h1 class="events-page-title">Group Up's &amp; Meetings</h1>
+				</div>
 
+				<p class="lead">Here is where you can check out our meeting topics with additional information. Make sure to put these important dates in your calendar. As for the meetings, you "Gotta catch 'em' all!", or at least try.</p>
+
+				<div class="grid-item events-articles">
+					<article>
+						<header class="events-headers">
+							<h2 class="events-h2">Class Introduction Presentation (12/6/2018)</h2>
+						</header>
+						<section class="post-content">
+							This week the founding members of our Pokémon club will be presenting their project 4 to their server-side scripting class with professor Fanfarelli. The presentation is one of many being presented in class that day, so come ready to learn. Presentations will begin at 8:00am sharp, so don't be late. 
+						</section>
+						<footer></footer>						
+					</article>
+				</div>
+				
+				<div class="grid-item events-articles">
+					<article>
+						<header class="events_headers">
+							<h2 class="events-h2">Post Presentation Meetup (12/09/2018)</h2>
+						</header>
+						<section class="post-content">
+							The sunday following the Class Introuction Presentation all members are welcome to group up and join us in a Pokémon hunt around UCF. Members are encouraged to bring thier phones with Pokémon Go lready installed and ready to use, otherwise, you'll have a very difficult time catching the Pokémon.
+						</section>
+						<footer></footer>						
+					</article>
+				</div>
+				
+				<div class="grid-item events-articles">
+					<article>
+						<header class="events-headers">
+							<h2 class="events-h2">To Be Announced</h2>
+						</header>
+						<section class="post-content">
+							These words here will be filled in later. Please come back at another time. 
+						</section>
+						<footer></footer>						
+					</article>
+				</div>
+				
+				<div class="grid-item events-articles">
+					<article>
+						<header class="events-headers">
+							<h2 class="events-h2">To Be Announced</h2>
+						</header>
+						<section class="post-content">
+							These words here will be filled in later. Please come back at another time. 
+						</section>
+						<footer></footer>
+					</article>
+				</div>
+				
+				<div class="grid-item events-articles">				
+					<article>
+						<header class="events-headers">
+							<h2 class="events-h2">To Be Announced</h2>
+						</header>
+						<section class="post-content">
+							These words here will be filled in later. Please come back at another time. 
+						</section>
+						<footer></footer>						
+					</article>
+				</div>
+			</div>
+			
+			<div class="events-sidebar">
+				<div class="grid-item events-images">
+					<img src="img/gengar.png" alt="gengar">
+				</div>
+				
+				<div class="events-sidebar"></div>
+			
+			
+			</div>
+			
+			<?php
+				require "includes/footer.php";
+			?>
+			
+		</div>	
+		
 	</body>  
 </html>	
